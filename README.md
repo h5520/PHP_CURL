c9eb88d77bb8466a0b17120d07232b25241c0665
h5520 <57217010+h5520@users.noreply.github.com>
2021-09-11 16:19:22 +0800

    2021年9月11日

--- README.md
+++ README.md
@@ -1,62 +1,18 @@
 # 使用方式
 
 ```bash
-composer require houzhonghua/weixinapi dev-main
+composer require houzhonghua/curl dev-main
 ```
 
 ## 引入
 
 ```php
-use houzhonghua\weixinapi\Weixin;
+use houzhonghua\curl\Curl;
 ```
 
-## 获取 OpenID
+## 请求接口
 ```php
-$query = new Weixin;
-$res = $query->OpenID($jscode);
-print_r($res);
-```
-
-## 获取用户手机号
-```php
-$query = new Weixin;
-$res = $query->getMobile($sessionKey,$encryptedData,$iv);
-print_r($res);
-```
-
-## 创建支付订单
-```php
-$query = new Weixin();
-$data = [
-    'out_trade_no' => "123123", # 订单号
-    'openid' => "openid", # openid
-    'price' => "0.01", # 价格
-    'notify' => "test", # 支付界面显示文字
-    'attach' => "test", # 额外参数
-    'notify_url' => "test", # 回调地址
-];
-$res = $query->pay($data);
-print_r($res);
-```
-
-## 微信退款
-```php
-$query = new Weixin();
-$out_trade_no = "123123"; # 订单号
-$transaction_id = "123123"; # 微信流水号
-$total_fee = 0.01; # 当前订单的总支付金额
-$refund_fee = 2; # 退款金额
-$res = $query->refund($out_trade_no,$transaction_id,$total_fee,$refund_fee);
-print_r($res);
-```
-
-## 微信提现
-```php
-$query = new Weixin();
-$fee = 0.01; # 提现金额
-$openid = "123123"; # openid
-$out_trade_no = "123123"; # 创建订单号
-$act_name = "提现"; # 描述
-$res = $query->Cashout($fee,$openid,$act_name);
+$query = new Curl($url,$method,$header,$https,$timeout);
+$res = $query->curl($data);
 print_r($res);
 ```
---
+++ src/Curl.php
@@ -0,0 +1,95 @@
+<?php
+namespace houzhonghua\curl;
+
+class Curl
+{
+	// url
+	private $url;
+	// 请求方式
+	private $method = "POST";
+	// 请求头信息
+	private $header = ["content-type: application/json"];
+	// https 请求
+	private $https = false;
+	// 请求时间，超过这个时间自动断开请求
+	private $timeout = 60;
+
+	/**
+	 * 初始化方法
+	 * @Author   HOU
+	 * @DateTime 2021-09-11T16:11:42+0800
+	 * @param    [type]                   $url
+	 * @param    [type]                   $method
+	 * @param    [type]                   $header
+	 * @param    [type]                   $https
+	 * @param    [type]                   $timeout
+	 */
+	public function __construct($url,$method,$header,$https,$timeout)
+	{
+		$this->url = $url;
+		$this->method = $method;
+		$this->header = $header;
+		$this->https = $https;
+		$this->timeout = $timeout;
+	}
+
+	/**
+	 * Curl
+	 * @Author   HOU
+	 * @DateTime 2021-09-11T16:12:18+0800
+	 * @param    array                    $data
+	 */
+	public function curl($data = [])
+	{
+		$method = strtoupper($this->method);
+        // 初始化
+        $ch = curl_init();
+        // 访问的URL
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        // 只获取页面内容，但不输出
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        if($this->https){
+
+            // https请求 不验证证书
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            // https请求 不验证 HOST
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        }
+
+        if ($method != "GET"){
+
+            if($method == 'POST'){
+                // 请求方式为post请求
+                curl_setopt($ch, CURLOPT_POST, true);
+            }
+
+            if($method == 'PUT' || strtoupper($method) == 'DELETE'){
+                // 设置请求方式
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+            }
+
+            // 请求数据
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+        // 模拟的header头
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->header);
+        // 设置不需要头信息
+        // curl_setopt($ch, CURLOPT_HEADER, false);
+        // 执行请求
+        $result = curl_exec($ch);
+
+        //  输出错误信息
+        if (curl_errno($ch)) {
+            return 'Curl error: ' . curl_error($ch);
+        }
+
+        // 关闭 curl，释放资源
+        curl_close($ch);
+
+        return $result;
+	}
+}
+?>
